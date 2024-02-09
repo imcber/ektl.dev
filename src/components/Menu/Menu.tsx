3da3579dc29dcbf91cfb9d7234a0f6a043acb20f
imcber <imcber1@gmail.com>
2024-02-09 00:04:17 -0600

    refactor home view

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,13 +2,16 @@ import { useTranslation } from "@/app/i18n";
 import React from "react";
 import Link from "next/link";
 import { useLanguage } from "@/context/LanguageContext";
+import Image from "next/image";
+
+import RightArrow from "../../../public/right-arrow-principal-color.svg";
 
 interface Props {}
 
 export const Menu = async ({}: Props) => {
 	const { language } = useLanguage();
 	const { t, languages } = await useTranslation({ language, ns: "menu" });
-	const itemsMenu = ["about", "work", "stack", "contact"];
+
 	return (
 		<div className='flex min-h-screen relative'>
 			<div className='w-full flex flex-col z-10 p-7 lg:p-14'>
@@ -16,13 +19,32 @@ export const Menu = async ({}: Props) => {
 					<span className='text-2xl'>ektl.dev</span>
 					<span className='text-2xl text-secondary '>
 						{languages.map((language) => (
-							<Link key={language} className='mr-4 cursor-pointer hover:text-foreground uppercase' href={`/${language}`}>
+							<Link key={language} className='mr-4 cursor-pointer hover:text-principal uppercase' href={`/${language}`}>
 								[ {language} ]
 							</Link>
 						))}
 					</span>
 				</div>
-				<div className='flex flex-col justify-center h-full pl-1 md:pl-14 lg:pl-28'></div>
+				<div className='flex flex-col justify-center items-center h-full'>
+					<h1 className='text-5xl my-3 text-center text-foreground font-bold [-webkit-text-stroke-width:2px] [-webkit-text-stroke-color:--secondary_light]'>
+						<span>{t("hello")}</span>
+						<span className='[-webkit-text-stroke-width:2px] text-secondary_light font-semibold'>{t("name")}</span>
+						<span>{t("rest")}</span>
+						<span className='[-webkit-text-stroke-width:2px] text-secondary_light font-semibold'>{t("alias")}</span>
+					</h1>
+					<h2 className='text-xl text-principal w-2/6 text-center'>{t("i_am")}</h2>
+
+					<div className='text-xl my-6 text-principal text-center w-2/6 flex justify-center'>
+						<a className='cursor-pointer flex mx-3'>
+							<Image src={RightArrow} width={20} alt='mail' className='' />
+							<span>{t("projects")}</span>
+						</a>
+						<a className='cursor-pointer flex mx-3 '>
+							<Image src={RightArrow} width={20} alt='mail' className='' />
+							<span>{t("about_me")}</span>
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
